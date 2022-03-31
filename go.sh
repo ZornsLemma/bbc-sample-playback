930f3ec5438e38b5153055f304355f821520ea38
Steven Flintham <sgf@lemma.co.uk>
2022-03-31 19:44:31 +0100

    First cut at pcmenc use
    Doesn't play back that well but it does kind of work so committing
    before I lose it

--- go.sh
+++ go.sh
@@ -3,3 +3,5 @@ set -e
 # ../wav_to_psg/wav_to_psg sine-440hz-8khz-mono.wav 
 beebasm -do exp1.ssd -i exp1.beebasm -v > exp1.lst
 cp exp1.ssd ~/shared/bbc-discs-tmp
+beebasm -do exp2.ssd -i exp2.beebasm -v > exp2.lst
+cp exp2.ssd ~/shared/bbc-discs-tmp

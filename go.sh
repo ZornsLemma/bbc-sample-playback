4e76ccc3c6f1be7ceb85327e94f2751d602890f8
Steven Flintham <sgf@lemma.co.uk>
2022-04-01 01:55:25 +0100

    First cut at interrupt-driven playback
    Sorta-kinda works

--- go.sh
+++ go.sh
@@ -5,3 +5,5 @@ beebasm -do exp1.ssd -i exp1.beebasm -v > exp1.lst
 cp exp1.ssd ~/shared/bbc-discs-tmp
 beebasm -do exp2.ssd -i exp2.beebasm -v > exp2.lst
 cp exp2.ssd ~/shared/bbc-discs-tmp
+beebasm -do exp3.ssd -i exp3.beebasm -v > exp3.lst
+cp exp3.ssd ~/shared/bbc-discs-tmp
